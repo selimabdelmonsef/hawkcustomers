51d13e0984e4ad838a3cacc6957b54ae6a8702d5
Selim <selimabdelmonsef@gmail.com>
2021-10-31 22:37:42 +0200

    refactored api call to Object, fixed delete,edit,add features

--- src/state/redux-actions/customerAction.js
+++ src/state/redux-actions/customerAction.js
@@ -1,14 +1,23 @@
 import { REDUCERS_CONSTANTS } from "../../constants/reducers.constants";
+import { convertArrayToObject } from "../../helper/util";
 
 export const selectCustomer = (payload) => ({
     type: REDUCERS_CONSTANTS.CUSTOMER.SELECT_CUSTOMER,
     data: payload
 });
 
-export const getCustomers = (payload) => ({
-    type: REDUCERS_CONSTANTS.CUSTOMER.GET_CUSTOMER_DATA,
-    data: payload
-});
+// export const getCustomers = (payload) => ({
+//     type: REDUCERS_CONSTANTS.CUSTOMER.GET_CUSTOMER_DATA,
+//     data: payload
+// });
+
+export const getCustomers = (payload) => {
+    const convertToKeydObj = convertArrayToObject(payload, 'id');
+    return {
+            type: REDUCERS_CONSTANTS.CUSTOMER.GET_CUSTOMER_DATA,
+            data: convertToKeydObj
+        };
+}
 
 export const addCustomer = (payload) => ({
     type: REDUCERS_CONSTANTS.CUSTOMER.ADD_CUSTOMER,
